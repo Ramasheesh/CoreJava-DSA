8c99e1a6ca5bbeaa9729a6d1f9be6c5647faf198
RamAsheesh Chauhan <rch231198@gmail.com>
2023-06-22 14:39:05 +0530

    function done

---
+++ function/sum.java
@@ -0,0 +1,24 @@
+
+import java.util.*;
+public class sum {
+  public static void sumOperation() {
+   int a = 10;
+   int b = 20;
+   int c = a+b;
+   System.out.println(c);
+  }
+  public static void sumByparams(int a  , int b) {
+    System.out.println(a+b);
+  }
+ public static void main(String[] args) {
+  System.out.println("hello");
+  sumOperation();
+  sumByparams(10 ,100);
+    // sumOperation();
+
+    //   sumOperation();
+
+    System.out.println("sir");
+
+ }
+}
