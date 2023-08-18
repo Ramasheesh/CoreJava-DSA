1846b3cbdeeaff5087eddcbadf9fc290671701fe
RamAsheesh Chauhan <43158746+Ramasheesh@users.noreply.github.com>
2023-08-18 20:16:12 +0530

    LTS :zap: Xor sum infosysDSA

---
+++ array/ArrayLTS.java
@@ -0,0 +1,25 @@
+class ArrayLTS
+{
+    public static int LIS(int[] arr, int i, int n, int prev)
+    {
+        if (i == n) {
+            return 0;
+        }
+        int excl = LIS(arr, i + 1, n, prev);
+
+        int incl = 0;
+        if (arr[i] > prev) {
+            incl = 1 + LIS(arr, i + 1, n, arr[i]);
+        }
+
+        return Integer.max(incl, excl);
+    }
+
+    public static void main(String[] args)
+    {
+        int[] arr = { 15, 6, 5,24,78,49,80,99, 12, 1 };
+
+        System.out.print("The length of the LIS is "
+                        + LIS(arr, 0, arr.length, Integer.MIN_VALUE));
+    }
+}
